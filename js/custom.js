9659f9a7f5f9e8a2f8ceb1adcf343a1b460ac802
Casey Utke <caseyutke@icloud.com>
2019-01-19 11:01:50 -0600

    Added Smooth Scrolling to contacts

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,9 @@
 function toggleDarkLight() {
   var $body = $("body");
   $body.toggleClass("dark-mode light-mode")
-}
+};
 
-//Toggles the text for Light Mode/Dark Mode
+//Toggles the text for Light Mode/Dark
 $('#mode').on('click', function(){
     $(this).html($(this).html() == 'Light Mode' ? 'Dark Mode' : 'Light Mode');
 });
