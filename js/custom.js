81c9e06beea277b6c7fb75fc715220334ee05dfe
Casey Utke <caseyutke@icloud.com>
2019-01-18 22:51:11 -0600

    Added Dark Mode Toggle

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+//Toggles between dark and light mode
+function toggleDarkLight() {
+  var $body = $("body");
+  $body.toggleClass("dark-mode light-mode")
+}
+
+//Toggles the text for Light Mode/Dark Mode
+$('#mode').on('click', function(){
+    $(this).html($(this).html() == 'Light Mode' ? 'Dark Mode' : 'Light Mode');
+});
