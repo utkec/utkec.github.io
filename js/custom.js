44422c1c21da91af1a1e29b1c2b83558d850b0f3
Casey Utke <caseyutke@icloud.com>
2019-01-19 15:57:11 -0600

    Tried different smooth scroll javascript

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,21 @@ function toggleDarkLight() {
 $('#mode').on('click', function(){
     $(this).html($(this).html() == 'Light Mode' ? 'Dark Mode' : 'Light Mode');
 });
+
+
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+});
