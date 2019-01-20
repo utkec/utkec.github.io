17898224e148ea96283ad61a0bdea3eb4fcb1eb4
Casey Utke <caseyutke@icloud.com>
2019-01-19 18:59:34 -0600

    Fixed Smooth scrolling bug and removed blue border on small screens

--- js/custom.js
+++ js/custom.js
@@ -9,20 +9,8 @@ $('#mode').on('click', function(){
     $(this).html($(this).html() == 'Light Mode' ? 'Dark Mode' : 'Light Mode');
 });
 
-
-$(function() {
-  // This will select everything with the class smoothScroll
-  // This should prevent problems with carousel, scrollspy, etc...
-  $('.smoothScroll').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000); // The number here represents the speed of the scroll in milliseconds
-        return false;
-      }
-    }
-  });
+$('.scroll').click(function(){
+  $('html body').animate({
+scrollTop: $(".contact7").offset().top
+}, 800);
 });
